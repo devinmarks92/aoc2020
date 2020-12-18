f66c7b7d260511f868fbb42472e7f708969ff551
Devin Marks <devinmarks92@gmail.com>
2020-12-18 11:53:48 -0800

    clean

--- src/aoc2020/day17.clj
+++ src/aoc2020/day17.clj
@@ -1,44 +1,46 @@
 (ns aoc2020.day17
-  (:require [clojure.string :as s]))
+  (:require [clojure.string :as s]
+            [clojure.math.combinatorics :refer [cartesian-product]]))
 
 (def input (->> (slurp "resources/test/day17")
                 s/split-lines
                 (mapv (fn [r] (mapv #(= \# %) r)))))
 
-;; generate n-dimensional coords: (vec (concat [x y] (repeat (- n 2) 0)))
-(def coord-props
-  {:bounds [[-1 (count input)] [-1 (count (first input))] [-1 1] [-1 1]]
-   :coords (into #{} (for [x (range (count input))
-                           y (range (count (first input)))
-                           :when (get-in input [x y])]
-                       [x y 0 0]))})
-
-(def dirs (remove #{[0 0 0 0]} (for [x [-1 0 1] y [-1 0 1] z [-1 0 1] w [-1 0 1]] [x y z w])))
+(defn gen-coord-props [dims]
+  (let [i (count input) j (count (first input))]
+    {:dims dims
+     :bounds (concat [[-1 i] [-1 j]] (repeat (- dims 2) [-1 1]))
+     :dirs (->> (apply cartesian-product (repeat dims [-1 0 1]))
+                (remove #{(repeat dims 0)}))
+     :coords (set (for [x (range i) y (range j) :when (get-in input [x y])]
+                    (vec (concat [x y] (repeat (- dims 2) 0)))))}))
 
 (defn get-submatrix
-  [[[x-floor x-ceil] [y-floor y-ceil] [z-floor z-ceil] [w-floor w-ceil]]]
-  (for [x (range x-floor (inc x-ceil))
-        y (range y-floor (inc y-ceil))
-        z (range z-floor (inc z-ceil))
-        w (range w-floor (inc w-ceil))]
-    [x y z w]))
+  [dims bounds]
+  (apply cartesian-product (map (fn [[i j]] (range i (inc j))) bounds)))
 
-(defn count-neighbors [coords coord]
+(defn count-neighbors [coords coord dirs]
   (->> (mapv #(mapv + coord %) dirs) (map coords) (remove nil?) count))
 
-(defn activate-coord [coords coord prev]
-  (let [n (count-neighbors prev coord), active? (prev coord)]
+(defn activate-coord
+  [curr-coords coord {:keys [coords dirs]}]
+  (let [n (count-neighbors coords coord dirs) active? (coords coord)]
     (if active?
-      (if (>= 3 n 2) coords (disj coords coord))
-      (if (= 3 n) (conj coords coord) coords))))
+      (if (>= 3 n 2) curr-coords (disj curr-coords coord))
+      (if (= 3 n) (conj curr-coords coord) curr-coords))))
 
 (defn update-coords
-  [{:keys [bounds coords] :as coord-props}]
-  (let [submatrix (get-submatrix bounds)
-        coords (reduce #(activate-coord %1 %2 coords) coords submatrix)]
-    {:coords coords
-     :bounds (apply mapv (juxt (comp dec min) (comp inc max)) coords)}))
+  [{:keys [dims dirs bounds coords] :as coord-props}]
+  (let [submatrix (get-submatrix dims bounds)
+        coords (reduce #(activate-coord %1 %2 coord-props) coords submatrix)]
+    (assoc coord-props
+           :coords coords
+           :bounds (apply mapv (juxt (comp dec min) (comp inc max)) coords))))
 
-(defn part1 []
-  (count (:coords (last (take 7 (iterate update-coords coord-props))))))
+(defn simulate [n coord-props]
+  (count (:coords (last (take (inc n) (iterate update-coords coord-props))))))
+
+(defn part1 [] (simulate 6 (gen-coord-props 3)))
+
+(defn part2 [] (simulate 6 (gen-coord-props 4)))
 
